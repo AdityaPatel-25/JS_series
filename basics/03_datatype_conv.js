3a608b47384f53ee1429462b76fdc7de4573e4e9
Aditya Lekhram Patel <pateladityanov.25@gmail.com>
2025-12-23 17:35:07 +0000

    datatype conversion done

---
+++ basics/03_datatype_conv.js
@@ -0,0 +1,14 @@
+let score = "33abc"
+
+// console.log("score:", score, typeof score);
+let valNum = Number(score)
+// console.log("valNum:", valNum, typeof valNum);
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log("booleanIsLoggedIn:", booleanIsLoggedIn, typeof booleanIsLoggedIn);
+
+// convert number to string
+let score2 = 44
+let scoreStr = String(score2)
+console.log("scoreStr:", scoreStr, typeof scoreStr);
