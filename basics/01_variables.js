0c1f89f415a0b1b3ad4696809f91d0f42aa7cb3b
Aditya Lekhram Patel <pateladityanov.25@gmail.com>
2025-12-23 17:17:04 +0000

    Container config done

---
+++ basics/01_variables.js
@@ -0,0 +1,10 @@
+const accId = 163258
+let accEmail = "hitesh@google.com"
+var accPass ="09876"
+accCity = "jaipur"
+
+//accId = 4  //not allowed
+
+// prefer not to use var
+// bcoz of issue in block scope and functional scope
+console.log(accId);
