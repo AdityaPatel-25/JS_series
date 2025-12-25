d649c74f31e41368ea8f58825706c899b5edec6c
Aditya Lekhram Patel <pateladityanov.25@gmail.com>
2025-12-25 16:10:56 +0000

    strings done

---
+++ basics/04_strings.js
@@ -0,0 +1,14 @@
+const name = "Aadi"
+const rep = 15
+
+// console.log(`Hello my name is ${name} and my rep is ${rep}`);  
+
+const gname = new String("Aadi25")
+// console.log(gname);
+
+// console.log(gname.length);
+// console.log(gname.toUpperCase());
+// console.log(gname.charAt(4));
+//console.log(gname.indexOf("d"));
+
+console.log(gname.slice(-6,5));
