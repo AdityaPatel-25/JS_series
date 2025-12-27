8b9ebc6c2d373d7de9a44a079d5c64aecb76954e
Aditya Lekhram Patel <pateladityanov.25@gmail.com>
2025-12-27 12:48:54 +0000

    math and num

---
+++ basics/05_nums_maths.js
@@ -0,0 +1,25 @@
+const score = 643
+// console.log(score);
+
+const bal = new Number(53274)
+// console.log(bal);
+
+// console.log(bal.toString().length);
+// console.log(bal.toFixed(2));
+
+//------------------------Maths------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-45));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 8, -1, 0, 34, 2));
+// console.log(Math.max(4, 8, -1, 0, 34, 2));
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor((Math.random()*10) + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
