d1f7884f74ea2fdd992862c1194778a9852f6166
Aditya Lekhram Patel <pateladityanov.25@gmail.com>
2025-12-29 18:39:27 +0000

    more on objects

---
+++ basics/objects-2.js
@@ -0,0 +1,40 @@
+const tinderUser = {};
+
+tinderUser.name = "John Doe";
+tinderUser.age = 28;
+tinderUser.isLoggedIn = true;
+
+//console.log(tinderUser);
+const regularUser = {
+    email: "john@example.com",
+    fullname: {
+        userFullname:  {
+            firstname: "John Doe",
+            lastname: "Smith"
+        }       
+    }
+}
+//console.log(regularUser.fullname.userFullname.firstname);       
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "c", 4: "d"};
+
+//const obj3 = Object.assign({}, obj1, obj2);
+const obj3 = {...obj1, ...obj2};
+//console.log(obj3);
+
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+//Destructuring
+const course = {
+    coursename: "JavaScript",
+    price: 999,
+    courseInstructor: "Aadi"
+}
+
+const {courseInstructor: instructor} = course;
+console.log(instructor);
