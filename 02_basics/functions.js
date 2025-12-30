e0b22ca6b78c3b3a92aabbedd915e347573b43b0
Aditya Lekhram Patel <pateladityanov.25@gmail.com>
2025-12-30 18:30:44 +0000

    functions done

---
+++ 02_basics/functions.js
@@ -0,0 +1,48 @@
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+const sum = addTwoNumbers(5, 7);
+//  console.log(sum);
+
+ function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("aditya"))
+// console.log(loginUserMessage("aditya"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "aditya",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
