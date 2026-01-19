c5f822c0236d1ee865f0736fd329823c98ef7aee
Aditya Lekhram Patel <pateladityanov.25@gmail.com>
2026-01-19 18:09:47 +0000

    reduce in js

---
+++ iterations/reduce.js
@@ -0,0 +1,19 @@
+const myNums = [1,2,3,4,5]
+
+// const myTotal = myNums.reduce(function (acc,currVal) {
+//     console.log(`acc: ${acc}  currVal: ${currVal}`);
+//     return acc + currVal;
+// }, 0);
+
+const myTotal = myNums.reduce( (acc, currVal) => acc + currVal , 0);
+
+console.log(myTotal); // 15
+
+// const product = {
+//     name: 'Laptop',
+//     price: 1000,
+//     discount: 0.1
+
+// };
+
+// }
